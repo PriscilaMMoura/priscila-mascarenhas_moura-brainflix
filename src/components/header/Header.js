a83b9b7f8a1379a38c63550716ab5abc839a5811
Priscila M. Moura <priscila.moura@gmail.com>
2022-11-02 16:45:17 -0700

    nav-bar markup ready

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,1 +1,40 @@
-import "./header.scss";
+import "./Header.scss";
+import logo from "../../assets/logo/BrainFlix-logo.svg";
+
+export default function Header() {
+  return (
+    <header>
+      <div className="nav-bar">
+        <div className="nav-bar__logo-container">
+          <img className="nav-bar__logo" src={logo} alt="BrainFlix logo" />
+        </div>
+        <div className="nav-bar__search-container">
+          <form className="nav-bar__form" action="submit">
+            <div className="nav-bar__search-avatar-wrap">
+              <input
+                className="nav-bar__input"
+                type="text"
+                placeholder="Search"
+              />
+              <label className="nav-bar__avatar-container--small">
+                <img
+                  className="nav-bar__avatar"
+                  src="../../assets/images/Mohan-muruge.jpg"
+                  alt="user avatar"
+                />
+              </label>
+            </div>
+            <button className="nav-bar__buttton">UPLOAD</button>
+          </form>
+          <div className="nav-bar__avatar-container--medium">
+            <img
+              className="nav-bar__avatar"
+              src="../../assets/images/Mohan-muruge.jpg"
+              alt="user avatar"
+            />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
