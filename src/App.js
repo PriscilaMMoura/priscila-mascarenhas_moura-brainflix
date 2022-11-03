16f570fc2d4228d600499c1096772c3ee6071c38
Priscila M. Moura <priscila.moura@gmail.com>
2022-11-03 15:17:21 -0700

    moved clickHandler and get data functions to App component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
 import Header from "./components/header/Header";
 import MainContent from "./components/maincontent/MainContent";
+import videoDetailsData from "./data/videoDetails.json";
+import videosData from "./data/videos.json";
+import { useState } from "react";
+
+function getVideos(videoId) {
+  return videosData.filter((video) => video.id !== videoId);
+}
+function getVideoDetails(videoId) {
+  return videoDetailsData.find((video) => video.id === videoId);
+}
 
 function App() {
+  const defaultVideoId = videosData[0].id;
+  // console.log(defaultVideoId);
+  const [videoId, setVideoId] = useState(defaultVideoId);
+  const [videos, setVideos] = useState(getVideos(videoId));
+  const [videoDetails, setVideoDetails] = useState(getVideoDetails(videoId));
+
+  const clickHandler = (event, videoId) => {
+    event.preventDefault();
+    setVideoId(videoId);
+    setVideos(getVideos(videoId));
+    setVideoDetails(getVideoDetails(videoId));
+  };
+
   return (
     <div className="app-container">
       <header>
         <Header />
       </header>
       <main>
-        <MainContent />
+        <MainContent
+          videos={videos}
+          videoDetails={videoDetails}
+          onMainContentClick={clickHandler}
+        />
       </main>
     </div>
   );
--- src/components/maincontent/MainContent.js
+++ src/components/maincontent/MainContent.js
@@ -3,34 +3,12 @@ import Hero from "../hero/Hero";
 import VideoDetails from "../videodetails/VideoDetails";
 import Comments from "../comments/Comments";
 import NextVideosList from "../nextvideoslist/NextVideosList";
-import videoDetailsData from "../../data/videoDetails.json";
-import videosData from "../../data/videos.json";
-import { useState } from "react";
 
-function getVideos(videoId) {
-  return videosData.filter((video) => video.id !== videoId);
-}
-function getVideoDetails(videoId) {
-  return videoDetailsData.find((video) => video.id === videoId);
-}
-
-export default function MainContent() {
-  const defaultVideoId = videosData[0].id;
-  // console.log(defaultVideoId);
-
-  const [videoId, setVideoId] = useState(defaultVideoId);
-  const [videos, setVideos] = useState(getVideos(videoId));
-  const [videoDetails, setVideoDetails] = useState(getVideoDetails(videoId));
-
-  const clickHandler = (event, videoId) => {
-    event.preventDefault();
-
-    console.log(event);
-    setVideoId(videoId);
-    setVideos(getVideos(videoId));
-    setVideoDetails(getVideoDetails(videoId));
-  };
-
+export default function MainContent({
+  videos,
+  videoDetails,
+  onMainContentClick,
+}) {
   return (
     <>
       <section className="hero-container">
@@ -41,7 +19,7 @@ export default function MainContent() {
         <Comments />
       </section>
       <section className="next-videos">
-        <NextVideosList videos={videos} onVideoListClick={clickHandler} />
+        <NextVideosList videos={videos} onVideoListClick={onMainContentClick} />
       </section>
     </>
   );
